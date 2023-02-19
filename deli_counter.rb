ae57874c4c247060ad29bef012265332438c72c2
ianrich69420 <ian.orieko@student.moringaschool.com>
2023-02-20 00:44:55 +0300

    commit tree bccf3c290a1fd7406fa1ce82a74a00eb0cdb013f

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def line array
+    if array.length() == 0
+        puts "The line is currently empty."
+    else
+        line = array.map.with_index { |value, index| "#{index + 1}. #{value}" }
+        puts "The line is currently: #{line.join(" ")}"
+    end
+end
+
+def take_a_number array, person
+    array.push(person)
+    puts "Welcome, #{person}. You are number #{array.index(person) + 1} in line."
+end
+
+def now_serving array
+    if array.length != 0
+        puts "Currently serving #{array.first()}."
+        array.shift()
+    else
+        puts "There is nobody waiting to be served!"
+    end
+end
